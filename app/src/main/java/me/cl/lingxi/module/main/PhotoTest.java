a2d0c49837a3493d1b988797c0a44b544e4390ad
Nickel-Angel <1239004072@qq.com>
2024-03-09 10:19:48 +0800

    fix many files which should not be empty

---
+++ app/src/main/java/me/cl/lingxi/module/main/PhotoTest.java
@@ -0,0 +1,14 @@
+package me.cl.lingxi.module.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import me.cl.lingxi.R;
+
+public class PhotoTest extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.__picker_activity_photo_picker);
+    }
+}
